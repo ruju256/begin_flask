9f602c656ba0045de51e55bb64e18f23d19d16cc
Mahlon <ezramahlon@gmail.com>
2018-12-11 11:07:22 +0300

    Added a check to LIMIT adding of other users to ONLY administrators

--- views/routes.py
+++ views/routes.py
@@ -54,26 +54,31 @@ def login():
 
 @app.route('/auth/signup', methods=['POST'])
 @token_required
-def signup():
+def signup(current_user):
+    
     post_data = request.json
     first_name = post_data['first_name']
     last_name = post_data['last_name']
     email = post_data['email']
     hashed_password = generate_password_hash(post_data['password'], method='sha256')
     role = post_data['role']
-    new_user = Users(first_name, last_name, email, hashed_password, role)
-    valid_user = new_user.validate_input()
-    if type(valid_user) is tuple:
-            return valid_user
-    else:
-        new_user.save_user()
-        return jsonify({
-            'msg': '{} successfully saved'.format(first_name),
-            'user': {
-                    "first_name": first_name,
-                    "last_name": last_name,
-                    "email": email,
-                    "password": hashed_password,
-                    "role": role
-                    },
-            }), 201
+    if current_user[5] != 'Admin':
+        return make_response("Only Administrators are allowed to register new members", 400)
+    else:    
+        print(current_user[5])
+        new_user = Users(first_name, last_name, email, hashed_password, role)
+        valid_user = new_user.validate_input()
+        if type(valid_user) is tuple:
+                return valid_user
+        else:
+            new_user.save_user()
+            return jsonify({
+                'msg': '{} successfully saved'.format(first_name),
+                'user': {
+                        "first_name": first_name,
+                        "last_name": last_name,
+                        "email": email,
+                        "password": hashed_password,
+                        "role": role
+                        },
+                }), 201
